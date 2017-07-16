55c58a34b0ce77e595f1819f81eb48850d07eae9
surjeet89 <surjeet.gfs@gmail.com>
2017-07-17 01:24:07 +0530

    Update mailgun-send-email.php

--- mailgun-send-email.php
+++ mailgun-send-email.php
@@ -5,14 +5,14 @@ $email_to = 'xxxxxx@gmail.com';
 $email_from = 'noreply@xxxxxx.com';
 $email_from_name = 'Quest Agency';
 $attachment = $_SERVER['DOCUMENT_ROOT'].'/images/upload.png';
-$email_cc = 'surjeet.gfs@gmail.com';
+$email_cc = 'xxxxx@gmail.com';
 $email_cc_name = 'surjeet';
 
 send_mail ($email_to, $subject, $message, $email_to_name , $email_from, $email_from_name, $email_cc, $email_cc_name, $email_bcc = '', $email_bcc_name = '',$attachment,$attachment_name='');
 
  function send_mail ($email_to, $subject, $message, $email_to_name = '', $email_from = '', $email_from_name = '', $email_cc = '', $email_cc_name = '', $email_bcc = '', $email_bcc_name = '',$attachment = '',$attachment_name = '') {
   
-  $url = 'https://api.mailgun.net/v3/mg.xxxx.yyyy/messages';  
+  $url = 'https://api.mailgun.net/v3/xxx.xxxx.xxxx/messages';  
 
   if(($email_cc == 'xxx@xxxx.xxxx')){
    $email_cc_name = 'xxxx';
@@ -23,7 +23,7 @@ send_mail ($email_to, $subject, $message, $email_to_name , $email_from, $email_f
    $email_bcc  = "xxx@xxxxx.com"; 
    $email_bcc_name  = "sean";
   }else{
-   //$email_bcc  = "ssurjeet@gmail.com"; 
+   //$email_bcc  = "surjeet@gmail.com"; 
    $email_bcc  = "surjeet@gmail.com"; 
    $email_bcc_name  = "sean"; 
   }
